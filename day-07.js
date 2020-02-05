821e094a00d8e28e6b7989de3b4b0f1abff8b268
eduardpapiian <eduardpapiian@gmail.com>
2020-02-05 22:05:58 +0200

    done hw 7

--- day-07.js
+++ day-07.js
@@ -1,25 +1,27 @@
 // Error Throwing - Error Handling #2 https://www.codewars.com/kata/55e7650c8d894146be000095/
 
-
 function validateMessage(msg) {
-    if(msg === null){
-        throw ReferenceError('Message is null!')
-    }else if(typeof msg !== 'string'){
-        throw TypeError(`Message should be of type string but was of type ${typeof msg}!`)
-    }else if(!msg.length || msg.length > 255){
-        throw RangeError(`Message contains ${msg.length} characters!`)
-    }else return !(/<.*>/.test(msg));
+  if (msg === null) {
+    throw ReferenceError("Message is null!");
+  } else if (typeof msg !== "string") {
+    throw TypeError(
+      `Message should be of type string but was of type ${typeof msg}!`
+    );
+  } else if (!msg.length || msg.length > 255) {
+    throw RangeError(`Message contains ${msg.length} characters!`);
+  } else return !/<.*>/.test(msg);
 }
 
-
 function validateMessage(msg) {
-    if(msg === null){
-        throw ReferenceError('Message is null!')
-    }else if(typeof msg !== 'string'){
-        throw TypeError(`Message should be of type string but was of type ${typeof msg}!`)
-    }else if(!msg.length || msg.length > 255){
-        throw RangeError(`Message contains ${msg.length} characters!`)
-    }else return !isHTML(msg);
+  if (msg === null) {
+    throw ReferenceError("Message is null!");
+  } else if (typeof msg !== "string") {
+    throw TypeError(
+      `Message should be of type string but was of type ${typeof msg}!`
+    );
+  } else if (!msg.length || msg.length > 255) {
+    throw RangeError(`Message contains ${msg.length} characters!`);
+  } else return !isHTML(msg);
 }
 
 const isHTML = RegExp.prototype.test.bind(/(<([^>]+)>)/i);
