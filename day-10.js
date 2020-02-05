c776705aac036d9438ac47d3423c7a313af5e09f
eduardpapiian <eduardpapiian@gmail.com>
2020-02-05 22:07:55 +0200

    done hw 10

--- day-10.js
+++ day-10.js
@@ -10,38 +10,39 @@ function alienLanguage(str) {
 
 // #20: Methods of String object--charAt() charCodeAt() and fromCharCode() http://www.codewars.com/kata/57284d23e81185ae6200162a
 
-function topSecret(str) {
-  const chars = str.split("");
-  for (let i = 0; i < chars.length; i++) {
-    switch (chars[i]) {
-      case "A":
-        chars[i] = "X";
-        break;
-      case "B":
-        chars[i] = "Y";
-        break;
-      case "C":
-        chars[i] = "Z";
-        break;
-      case "a":
-        chars[i] = "x";
-        break;
-      case "b":
-        chars[i] = "y";
-        break;
-      case "c":
-        chars[i] = "z";
-        break;
-      case " ":
-        continue;
-      case "!":
-        continue;
-      default:
-        chars[i] = String.fromCharCode(chars[i].charCodeAt() - 3);
-    }
-  }
-  return chars.join("");
-}
+//TODO
+// function topSecret(str) {
+//   const chars = str.split("");
+//   for (let i = 0; i < chars.length; i++) {
+//     switch (chars[i]) {
+//       case "A":
+//         chars[i] = "X";
+//         break;
+//       case "B":
+//         chars[i] = "Y";
+//         break;
+//       case "C":
+//         chars[i] = "Z";
+//         break;
+//       case "a":
+//         chars[i] = "x";
+//         break;
+//       case "b":
+//         chars[i] = "y";
+//         break;
+//       case "c":
+//         chars[i] = "z";
+//         break;
+//       case " ":
+//         continue;
+//       case "!":
+//         continue;
+//       default:
+//         chars[i] = String.fromCharCode(chars[i].charCodeAt() - 3);
+//     }
+//   }
+//   return chars.join("");
+// }
 
 // #21: Methods of String object--trim() and the string template http://www.codewars.com/kata/5729b103dd8bac11a900119e
 
