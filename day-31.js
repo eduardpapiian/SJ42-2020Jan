76d0e156a9116ade382517c991be57d197cfac5a
eduardpapiian <eduardpapiian@gmail.com>
2020-02-26 00:33:07 +0200

    done hw 31

--- day-31.js
+++ day-31.js
@@ -388,36 +388,123 @@ describe('Classes can inherit from another using `extends`', () => {
 // Follow the hints of the failure messages!
 
 describe('Classes can inherit from another', () => {
-  it('extend an `old style` "class", a function, still works', () => {
-    class A {}
-    class B extends A {}
-    assert.equal(new B() instanceof A, true);
-  });
+    it('extend an `old style` "class", a function, still works', () => {
+        class A {}
+        class B extends A {}
+        assert.equal(new B() instanceof A, true);
+    });
 
-  describe('prototypes are as you know them', () => {
-    class A {}
-    class B extends A {}
-    it('A is the prototype of B', () => {
-      const isIt = A.isPrototypeOf(B);
-      assert.equal(isIt, true);
+    describe('prototypes are as you know them', () => {
+        class A {}
+        class B extends A {}
+        it('A is the prototype of B', () => {
+            const isIt = A.isPrototypeOf(B);
+            assert.equal(isIt, true);
+        });
+        it('A`s prototype is also B`s prototype', () => {
+            const proto = B.prototype;
+            // Remember: don't touch the assert!!! :)
+            assert.equal(A.prototype.isPrototypeOf(proto), true);
+        });
     });
-    it('A`s prototype is also B`s prototype', () => {
-      const proto = B;
-      // Remember: don't touch the assert!!! :)
-      assert.equal(A.prototype.isPrototypeOf(proto), true);
-    });
-  });
 
-  describe('`extends` using an expression', () => {
-    it('e.g. the inline assignment of the parent class', () => {
-      let A;
-      class B extends (A = {}) {}
-      assert.equal(new B() instanceof A, true);
+    describe('`extends` using an expression', () => {
+        it('e.g. the inline assignment of the parent class', () => {
+            let A;
+            class B extends (A = class {}) {}
+            assert.equal(new B() instanceof A, true);
+        });
+        it('or calling a function that returns the parent class', () => {
+            const returnParent = (beNull) => beNull ? null : class {};
+            class B extends returnParent(1) {}
+            assert.equal(Object.getPrototypeOf(B.prototype), null);
+        });
     });
-    it('or calling a function that returns the parent class', () => {
-      const returnParent = (beNull) => beNull ? null : class {};
-      class B extends returnParent {}
-      assert.equal(Object.getPrototypeOf(B.prototype), null);
+});
+
+
+// Super in Method http://tddbin.com/#?kata=es6/language/class/super-in-method
+
+// 27: class - super inside a method
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('Inside a class use `super` to access parent methods', () => {
+    it('use of `super` without `extends` fails (already when transpiling)', () => {
+        class A {hasSuper() { return false; }}
+        assert.equal(new A().hasSuper(), false);
     });
-  });
+    it('`super` with `extends` calls the method of the given name of the parent class', () => {
+        class A {hasSuper() { return true; }}
+        class B extends A {hasSuper() { return super.hasSuper(); }}
+        assert.equal(new B().hasSuper(), true);
+    });
+    it('when overridden a method does NOT automatically call its super method', () => {
+        class A {hasSuper() { return true; }}
+        class B extends A {hasSuper() { }}
+        assert.equal(new B().hasSuper(), void 0);
+    });
+    it('`super` works across any number of levels of inheritance', () => {
+        class A {iAmSuper() { return this.super = true; }}
+        class B extends A {constructor() { super(); this.super = true; }}
+        class C extends B {iAmSuper() {
+            return super.iAmSuper();
+        }}
+        assert.equal(new C().iAmSuper(), true);
+    });
+    it('accessing an undefined member of the parent class returns `undefined`', () => {
+        class A {}
+        class B extends A {getMethod() { return super.undefined; }}
+        assert.equal(new B().getMethod(), void 0);
+    });
+});
+
+// Super in Constructor http://tddbin.com/#?kata=es6/language/class/super-in-constructor
+
+// 28: class - super in constructor
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('Inside a class`s constructor `super()` can be used', () => {
+    it('`extend` a class and use `super()` to call the parent constructor', () => {
+        class A {constructor() { this.levels = 1; }}
+        class B extends A {
+            constructor() {
+                super()
+                this.levels++;
+            }
+        }
+        assert.equal(new B().levels, 2);
+    });
+    it('`super()` may also take params', () => {
+        class A {constructor(startValue=1, addTo=1) { this.counter = startValue + addTo; }}
+        class B extends A {
+            constructor(...args) {
+                super(...args);
+                this.counter++;
+            }
+        }
+        assert.equal(new B(42, 2).counter, 45);
+    });
+    it('it is important where you place your `super()` call!', () => {
+        class A {inc() { this.countUp = 1; }}
+        class B extends A {
+            inc() {
+                this.countUp = 2;
+                super.inc();
+                return this.countUp;
+            }
+        }
+        assert.equal(new B().inc(), 1);
+    });
+    it('use `super.constructor` to find out if there is a parent constructor', () => {
+        class ParentClassA {constructor() {"parent"}}
+        class B extends ParentClassA {
+            constructor() {
+                super();
+                this.isTop = '' + super.constructor;
+            }
+        }
+        assert(new B().isTop.includes('ParentClassA'), new B().isTop);
+    });
 });
