aabb4de8fcc601ea432c3d3ecc9139558f3cc960
eduardpapiian <eduardpapiian@gmail.com>
2020-02-27 22:58:23 +0200

    hw 32 done

--- day-29.js
+++ day-29.js
@@ -4,3 +4,15 @@ let myObj = {};
 myObj.__proto__ = MyObject.prototype;
 myObj.contructor = MyObject.constructor;
 myObj.constructor();
+
+
+// Replicate new http://www.codewars.com/kata/replicate-new
+
+function nouveau(Constructor) {
+    const obj = {};
+    const arg = Array.prototype.slice.call(arguments, 1);
+    obj.__proto__ = Constructor.prototype;
+    obj.contructor = Constructor.constructor();
+    obj.constructor(arg[0], arg[1], arg[2], arg[3]);
+    return obj;
+}
