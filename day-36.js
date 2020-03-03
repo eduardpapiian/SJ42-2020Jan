22bbdbbfc5da2bc737c510945d160e4330134661
eduardpapiian <mikroffon@gmail.com>
2020-03-03 10:51:41 +0200

    done hw 36

--- day-36.js
+++ day-36.js
@@ -205,3 +205,212 @@ class NotRejectedError extends Error {
     this.message = 'Expected promise to be rejected.';
   }
 }
+
+// chaining `then()` http://tddbin.com/#?kata=es6/language/promise/chaining-then
+
+// 77: Promise - chaining
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('chaining multiple promises can enhance readability', () => {
+  describe('prerequisites for understanding', function() {
+    it('reminder: the test passes when a fulfilled promise is returned', function() {
+      return Promise.resolve('I should fulfill.');
+    });
+    it('a function given to `then()` fulfills (if it doesnt throw)', function() {
+      const beNice = () => 'I am nice';
+      return Promise.resolve()
+        .then(beNice)
+        .then(niceMessage => assert.equal(niceMessage, 'I am nice'));
+    });
+  });
+  describe('chain promises', function() {
+    const removeMultipleSpaces = s => s.replace(/\s+/g, ' ');
+    it('`then()` receives the result of the promise it was called on', function() {
+      const wordsPromise = Promise.resolve('one  space     between each     word');
+      return wordsPromise
+      // .then(s => removeMultipleSpaces())
+        .then(s => removeMultipleSpaces(s))
+        .then(actual => {assert.equal(actual, 'one space between each word')})
+        ;
+    });
+    const appendPeriod = s => `${s}.`;
+    it('multiple `then()`s can be chained', function() {
+      const wordsPromise = Promise.resolve('Sentence without       an end');
+      return wordsPromise
+        .then(str => removeMultipleSpaces(str))
+        .then(str => appendPeriod(str))
+        .then(actual => {assert.equal(actual, 'Sentence without an end.')})
+        ;
+    });
+    const trim = s => s.replace(/^\s+/, '').replace(/\s+$/, '');
+    it('order of the `then()`s matters', function() {
+      const wordsPromise = Promise.resolve('Sentence without       an end ');
+      return wordsPromise
+        .then(removeMultipleSpaces)
+        .then(trim)
+        .then(appendPeriod)
+        .then(actual => {assert.equal(actual, 'Sentence without an end.')})
+        ;
+    });
+    const asyncUpperCaseStart = (s, onDone) => {
+      const format = () => onDone(s[0].toUpperCase() + s.substr(1));
+      setTimeout(format, 100);
+    };
+    it('any of the things given to `then()` can resolve asynchronously (the real power of Promises)', function() {
+      const wordsPromise = Promise.resolve('sentence without an end');
+      return wordsPromise
+        .then(s => new Promise(resolve => asyncUpperCaseStart(s, resolve)))
+        .then(s => new Promise(resolve => setTimeout(() => resolve(appendPeriod(s)), 100)))
+        .then(actual => {assert.equal(actual, 'Sentence without an end.')})
+        ;
+    });
+    it('also asynchronously, the order still matters, promises wait, but don`t block', function() {
+      const wordsPromise = Promise.resolve('trailing space   ');
+      return wordsPromise
+        .then(s => new Promise(resolve => asyncUpperCaseStart(s, resolve)))
+        .then(s => new Promise(resolve => setTimeout(() => resolve(trim(s)), 100)))
+        .then(s => new Promise(resolve => setTimeout(() => resolve(appendPeriod(s)), 100)))
+        .then(actual => {assert.equal(actual, 'Trailing space.')})
+        ;
+    });
+  });
+});
+
+
+// the API http://tddbin.com/#?kata=es6/language/promise/api
+
+// 78: Promise - API overview
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`Promise` API overview', function() {
+  it('`new Promise()` requires a function as param', () => {
+    const param = () => {};
+    assert.doesNotThrow(() => { new Promise(param); });
+  });
+  describe('resolving a promise', () => {
+    // reminder: the test passes when a fulfilled promise is returned
+    it('via constructor parameter `new Promise((resolve) => { resolve(); })`', () => {
+      const param = (resolve) => { resolve(); };
+      return new Promise(param);
+    });
+    it('using `Promise.resolve()`', () => {
+      return Promise.resolve('all fine');
+    });
+  });
+  describe('a rejected promise', () => {
+    it('using the constructor parameter', (done) => {
+      const promise = new Promise((resolve, reject) => { reject(); });
+      promise
+        .then(() => done(new Error('The promise is expected to be rejected.')))
+        .catch(() => done());
+    });
+    it('via `Promise.reject()`', (done) => {
+      const promise = Promise.reject();
+      promise
+        .then(() => done(new Error('The promise is expected to be rejected.')))
+        .catch(() => done());
+    });
+  });
+  describe('`Promise.all()`', () => {
+    it('`Promise.all([p1, p2])` resolves when all promises resolve', () => {
+      return Promise.all([Promise.resolve(), Promise.resolve(), Promise.resolve()])
+    });
+    it('`Promise.all([p1, p2])` rejects when a promise is rejected', (done) => {
+      Promise.all([Promise.reject()])
+        .then(() => done(new Error('The promise is expected to be rejected.')))
+        .catch(() => done())
+    });
+  });
+  describe('`Promise.race()`', () => {
+    it('`Promise.race([p1, p2])` resolves/reject when one of the promises resolves/rejects', () => {
+      return Promise.race([Promise.resolve(), Promise.reject()])
+    });
+    it('`Promise.race([p1, p2])` rejects when one of the promises rejects', (done) => {
+      Promise.race([Promise.reject()])
+        .then(() => done(new Error('The promise is expected to be rejected.')))
+        .catch(() => done())
+    });
+    it('`Promise.race([p1, p2])` order matters (and timing)', () => {
+      return Promise.race([Promise.resolve(), Promise.reject()])
+    });
+  });
+});
+
+// promise.catch() http://tddbin.com/#?kata=es6/language/promise/catch
+
+// 79: Promise - catch
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+// Here we use promises to trigger, don't modify the block with the
+// returning promise!
+
+describe('`catch()` returns a Promise and deals with rejected cases only', () => {
+  describe('prerequisites for understanding', () => {
+    it('*return* a fulfilled promise, to pass a test', () => {
+      return Promise.resolve();
+      assert(false); // Don't touch! Make the test pass in the line above!
+    });
+    it('reminder: the test passes when a fulfilled promise is returned', () => {
+      return Promise.resolve('I should fulfill.');
+    });
+  });
+  describe('`catch` method basics', () => {
+    it('is an instance method', () => {
+      const p = Promise;
+      assert.equal(typeof p.prototype.catch, 'function');
+    });
+
+    it('catches only promise rejections', (done) => {
+      const promise = Promise.reject();
+      promise
+        .then(() => { done('Should not be called!'); })
+        .catch(done);
+    });
+
+    it('returns a new promise', () => {
+      const whatToReturn = () => Promise.resolve();
+      const promise = Promise.reject();
+      return promise.catch(() =>
+        whatToReturn()
+      );
+    });
+
+    it('converts it`s return value into a promise', () => {
+      const p = Promise.reject();
+      const p1 = p.catch(() => 'promise?');
+      return p1.then(result => assert.equal('promise?', result));
+    });
+
+    it('the first parameter is the rejection reason', () => {
+      const p = Promise.reject('oops');
+      return p.catch(reason => {
+        assert.equal(reason, 'oops');
+      });
+    });
+  });
+  describe('multiple `catch`es', () => {
+    it('only the first `catch` is called', () => {
+      const p = Promise.reject('1');
+      const p1 = p
+        .catch(reason => `${reason} AND 2`)
+        .catch(reason => `${reason} AND 3`)
+      ;
+      return p1.then(result =>
+        assert.equal(result, '1 AND 2')
+      );
+    });
+    it('if a `catch` throws, the next `catch` catches it', () => {
+      const p = Promise.reject('1');
+      const p1 = p
+        .catch(reason => { throw Error(`${reason} AND 2`) })
+        .catch(err => `${err.message} AND 3`)
+        .catch(err => `${err} but NOT THIS`)
+      ;
+      return p1.then(result =>
+        assert.equal(result, '1 AND 2 AND 3')
+      );
+    });
+  });
+});
