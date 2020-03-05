ef7c8a9a3d09b05e2e44457481d4b9c8511589e9
eduardpapiian <eduardpapiian@gmail.com>
2020-03-05 23:48:26 +0200

    fixed hw 39

--- day-39.js
+++ day-39.js
@@ -4,12 +4,15 @@ function createIterator(array){
     const iterator = {
         index: 0,
         next: function() {
-            if( iterator.index < array.length ) {
-                return { value: array[iterator.index++], done: false }
+            if (iterator.index < array.length) {
+                return {
+                    value: array[iterator.index++], done: false
+                }
             }
-            return { value: array[iterator.index], done: true }
+            return {
+                value: array[iterator.index], done: true
+            }
         }
-
     };
     return iterator
 }
